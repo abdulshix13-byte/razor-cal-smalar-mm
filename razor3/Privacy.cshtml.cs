745b4951b26850d10d4daf6fb6949e3cf9425e12
abdulhalim <abdulshix13@gmail.com>
2026-04-09 10:27:21 +0300

    Add files via upload

---
+++ razor3/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+        public string Mesaj { get; set; }
+        public void OnGet()
+        {
+            Mesaj = "Merhaba, şu an server zamanı: " + DateTime.Now.ToString();
+
+        }
+    }
+
+}
